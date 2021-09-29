97d7ced80def8c75016e1d4d0090e01ea789bb3f
Manu Harju <manu.harju@gmail.com>
2021-09-29 22:08:20 +0300

    eval tuning

--- 03_eval.py
+++ 03_eval.py
@@ -15,6 +15,7 @@ def main():
     cfg = load_settings()
 
     cache_dir = cfg.get('cache_dir', 'data_cache')
+    results_dir = cfg.get('results_dir', 'results')
 
     model_dir = cfg.get('model_dir', 'model')
     model_name = cfg.get('model_name', 'model')
@@ -22,21 +23,24 @@ def main():
 
     sequence_len = cfg.get('sequence_len', 5)
 
+    # load data & construct sequences
     eval_df = pd.read_csv(os.path.join(cache_dir, 'eval_data.csv'), index_col='Date')
     eval_data = np.array(eval_df)
-
     seqs = construct_sequences(eval_data, sequence_len)
 
+    # load omdel
     model = tf.keras.models.load_model(model_fn)
 
+    # compute reconstruction error
     seqs_pred = model(seqs)
-
     mse = np.mean(np.square(seqs_pred - seqs), axis=(1, 2))
 
-    print(mse.shape, len(eval_df))
-
+    # save results
+    os.makedirs(results_dir, exist_ok=True)
     error_df = pd.DataFrame(data={'mse': mse}, index=eval_df.index[sequence_len - 1:])
-    error_df.to_csv('error.csv')
+    error_fn = os.path.join(results_dir, model_name + '_error.csv')
+    error_df.to_csv(error_fn)
+
 
 if __name__ == '__main__':
     main()
